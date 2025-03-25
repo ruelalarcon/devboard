ff93eab28955b0f769804722d1f561194acaa8d9
Ruel Nathaniel Alarcon <ruelnalarcon@gmail.com>
2025-03-25 02:09:03 -0600

    Use default vite port for CORS

--- server/server.js
+++ server/server.js
@@ -64,7 +64,7 @@ async function startServer() {
   app.use(
     "/graphql",
     cors({
-      origin: process.env.CLIENT_URL || "http://localhost:3000",
+      origin: process.env.CLIENT_URL || "http://localhost:5173",
       credentials: true,
     }),
     express.json(),
